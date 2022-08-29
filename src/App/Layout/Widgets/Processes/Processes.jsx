9b8020c9b749b4351e03e898ef7ec49e3ae60e8a
Lucian <luciangabrielilie@gmail.com>
2022-08-29 23:02:32 +0100

    updated vscode js project configuration

--- src/App/Layout/Widgets/Processes/Processes.jsx
+++ src/App/Layout/Widgets/Processes/Processes.jsx
@@ -1,5 +1,5 @@
+import Table from "App/Layout/Gauges/Table/Table";
 import PropTypes from "prop-types";
-import Table from "../Gauges/Table/Table";
 import Card from "../Card/Card";
 
 const Processes = ({ data }) => <Card title="Top processes" content={<Table heading={Object.keys(data[0])} body={data} />} />
